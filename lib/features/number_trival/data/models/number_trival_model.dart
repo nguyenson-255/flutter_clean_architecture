c660c0ebfe56808a71af54a95d07515320dbc694
nguyenson-255 <nguyenson10b12@gmail.com>
2023-09-15 03:00:06 +0700

    P4 add test from json and to json

--- lib/features/number_trival/data/models/number_trival_model.dart
+++ lib/features/number_trival/data/models/number_trival_model.dart
@@ -1,6 +1,19 @@
+import 'dart:convert';
 import 'package:cleancodepart1/features/number_trival/domain/entities/NumberTrival.dart';
+import 'package:flutter/foundation.dart';
 
 class NumberTrivalModel extends NumberTrival {
   NumberTrivalModel({required super.text, required super.number});
 
+
+  factory NumberTrivalModel.fromJson(Map<String, dynamic> json) {
+    return NumberTrivalModel(text: json['text'], number: (json['number'] as num).toInt());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "text": text,
+      "number": number
+    };
+  }
 }
